b781549be0f62b9940ca987d7ce11bc37048be0b
menshikov <matveydrobot15@gmail.com>
2024-02-04 14:46:38 +0300

    Second commit

--- api_with_restrictions/advertisements/views.py
+++ api_with_restrictions/advertisements/views.py
@@ -1,15 +1,25 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
-
+from .models import Advertisement
+from .serializers import AdvertisementSerializer
+from .filters import AdvertisementFilter
 
 class AdvertisementViewSet(ModelViewSet):
-    """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    permission_classes = [IsAuthenticated]
+    filterset_class = AdvertisementFilter
 
-    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
-    #   сериализаторов и фильтров
+    def perform_update(self, serializer):
+        """Проверка на автора при обновлении объявления."""
+        if serializer.instance.creator == self.request.user:
+            serializer.save()
+        else:
+            raise PermissionError("Вы не являетесь автором этого объявления.")
 
-    def get_permissions(self):
-        """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
-            return [IsAuthenticated()]
-        return []
+    def perform_destroy(self, instance):
+        """Проверка на автора при удалении объявления."""
+        if instance.creator == self.request.user:
+            instance.delete()
+        else:
+            raise PermissionError("Вы не являетесь автором этого объявления.")
--- api_with_restrictions/api_with_restrictions/urls.py
+++ api_with_restrictions/api_with_restrictions/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from rest_framework.routers import DefaultRouter
+from .views import AdvertisementViewSet
 
 router = DefaultRouter()
-# TODO: подключите `AdvertisementViewSet`
-
+router.register(r'advertisements', AdvertisementViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
