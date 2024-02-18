6f353c20c7c8845b361e39c23326b93f509cfbd9
menshikov <matveydrobot15@gmail.com>
2024-02-18 17:43:20 +0300

    Fourth commit

--- api_with_restrictions/advertisements/views.py
+++ api_with_restrictions/advertisements/views.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 from .models import Advertisement
 from .permissions import IsAdvertisementCreator
@@ -8,19 +8,5 @@ from .filters import AdvertisementFilter
 class AdvertisementViewSet(ModelViewSet):
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAdvertisementCreator]
     filterset_class = AdvertisementFilter
-
-    def perform_update(self, serializer):
-        """Проверка на автора при обновлении объявления."""
-        if serializer.instance.creator == self.request.user:
-            serializer.save()
-        else:
-            raise PermissionError("Вы не являетесь автором этого объявления.")
-
-    def perform_destroy(self, instance):
-        """Проверка на автора при удалении объявления."""
-        if instance.creator == self.request.user:
-            instance.delete()
-        else:
-            raise PermissionError("Вы не являетесь автором этого объявления.")
